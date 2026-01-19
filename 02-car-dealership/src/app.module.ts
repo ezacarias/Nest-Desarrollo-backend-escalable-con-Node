ac3f2ff20a597413887fba5c37b93c985b7ef1fc
Edizon <edizon1987@gmail.com>
2026-01-18 22:17:50 -0600

    Update - Controladores

--- 02-car-dealership/src/app.module.ts
+++ 02-car-dealership/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CarsModule } from './cars/cars.module';
 @Module({
-  imports: [],
+  imports: [CarsModule],
   controllers: [],
   providers: [],
   exports:[]
