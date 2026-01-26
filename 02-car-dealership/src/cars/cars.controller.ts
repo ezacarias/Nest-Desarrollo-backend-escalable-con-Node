796f633f5fdd965d6b4c850544c927951a2655d9
Edizon <edizon1987@gmail.com>
2026-01-25 21:37:32 -0600

    Sección 04: Pipe - ParseUUIDPipe

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, ParseIntPipe, Body, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param, ParseIntPipe, Body, Patch, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -15,7 +15,7 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById(@Param('id') id:string){
+    getCarById(@Param('id', ParseUUIDPipe) id:string){
         return this.carsService.findOneById(id);
     }
 
